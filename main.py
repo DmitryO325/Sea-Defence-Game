93907f0508df999391373ec34287d36f9c667814
KrivosheyYuriy <Lonespy@yandex.ru>
2022-12-17 14:09:13 +0300

    Creating pre-window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame_widgets.button import Button
+import pygame_widgets
 
 
 class Window:
@@ -8,20 +9,25 @@ class Window:
         # width = pygame.display.Info().current_w
         # height = pygame.display.Info().current_h
         self.size = self.width, self.height = 800, 600
-        self.screen = pygame.display.set_mode(self.size, pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode(self.size)
+        self.running = True
+        self.objects = []
 
     def resize(self, x, y):
         self.width = x
         self.height = y
 
     def show(self):
-        self.running = True
         while self.running:
-            for event in pygame.event.get():
+            events = pygame.event.get()
+            for event in events:
                 if event.type == pygame.QUIT:
                     self.running = False
             pygame.display.flip()
-        pygame.quit()
+            pygame_widgets.update(events)
+            for element in self.objects:
+                element.listen(events)
+                element.draw()
 
     def hide(self):
         self.running = False
@@ -34,7 +40,7 @@ class MainWindow(Window):
                         fontSize=30,
                         colour='red', borderThickness=10, pressedColour=pygame.Color('yellow'),
                         hoverColour=pygame.Color('green'))
-        self.show()
+        self.objects.append(button)
 
 
 win = MainWindow()
