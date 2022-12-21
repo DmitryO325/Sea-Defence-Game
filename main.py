c75a78963c58d3807a4510dcd0ccbe28c51bb623
KrivosheyYuriy <Lonespy@yandex.ru>
2022-12-21 17:20:39 +0300

    class Main_menu created

--- main.py
+++ main.py
@@ -14,34 +14,57 @@ class Window:
 class MainWindow(Window):
     def __init__(self):
         super().__init__()
-
-        # pygame.mixer.music.set_volume(0.5)
-        # pygame.mixer.music.stop()
-        # pygame.mixer.music.pause()
-        # pygame.mixer.music.unpause()
         self.Win = None
-        self.button = Button(screen, 100, 100, 300, 150, text='Тестирование кнопки',
-                             textColour=pygame.Color('blue'),
-                             fontSize=30,
-                             colour='red', borderThickness=10, pressedColour='yellow',
-                             hoverColour=pygame.Color('green'))
-        self.objects.append(self.button)
+        self.button_titles = ('name', 'survival', 'level_mode', 'shipyard', 'top_players', 'options', 'exit')
+        self.button_functions = ()
+        for number_of_button in range(1, 7):  # создание кнопок для других окон
+            Button(
+                screen,
+                round(width * 0.2),
+                round(height * (0.06 + number_of_button * 0.13 + 0.03)),
+                round(width * 0.6),
+                round(height * 0.1),
+                colour='yellow', text=self.button_titles[number_of_button], textColour='red',
+                fontSize=60, radius=10, hoverColour='cyan', pressedColour='blue'
+            )
+        Button(  # создание кнопки для настройки пользователя
+            screen,
+            round(width * 0.885),
+            round(height * 0.03),
+            round(width * 0.1),
+            round(height * 0.05),
+            colour='green', text='username'
+        )
         while self.running:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     self.running = False
-                if self.button.clicked:
-                    self.new_window()
             pygame.display.flip()
             pygame_widgets.update(events)
             for element in self.objects:
                 element.listen(events)
                 element.draw()
 
-    def new_window(self):
+    def to_options(self):
         self.running = False
-        self.Win = MainWindow2()
+        # self.Win = MainWindow2()
+
+    def to_top_players(self):
+        self.running = False
+        # self.Win = MainWindow2()
+
+    def to_shipyard(self):
+        self.running = False
+        # self.Win = MainWindow2()
+
+    def to_level_mode(self):
+        self.running = False
+        # self.Win = MainWindow2()
+
+    def to_survival(self):
+        self.running = False
+        # self.Win = MainWindow2()
 
 
 class MainWindow2(Window):
@@ -79,3 +102,7 @@ if __name__ == '__main__':
     pygame.mixer.music.load('Audio/Background.mp3')
     pygame.mixer.music.play(-1)
     MainWindow()
+    # pygame.mixer.music.set_volume(0.5)
+    # pygame.mixer.music.stop()
+    # pygame.mixer.music.pause()
+    # pygame.mixer.music.unpause()
