dbcef79be96edcc6b01bee7d65a295fa1658e8b1
KrivosheyYuriy <Lonespy@yandex.ru>
2023-01-05 17:41:02 +0300

    test3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import pygame_widgets
 import sys
@@ -19,35 +21,35 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Ship(pygame.sprite.Sprite):
+class Ship(pygame.sprite.Sprite):  # класс корабля (общий для игрока и противников)
     def __init__(self, group):
         super().__init__(group)
-        self.armor = 0
-        self.speed = 0
+        self.armor = 0  # количество брони
+        self.speed = 0  # скорость корабля
 
-    def torpedo_shot(self, coords, group):
+    def torpedo_shot(self, coords, group):  # функция запуска торпеды
         if self.rect.x + self.rect.w * 0.5 > coords[0]:
             Torpedo(round(self.rect.x + 0.1 * self.rect.w), round(0.95 * self.rect.y), coords, group)
         else:
             Torpedo(round(self.rect.x + 0.8 * self.rect.w), round(0.95 * self.rect.y), coords, group)
 
-    def get_damage(self):
-        self.armor -= 80
+    def get_damage(self, damage):  # функция получения урона
+        self.armor -= damage
 
 
-class Player(Ship):
+class Player(Ship):  # класс игрока
     def __init__(self, group):
         super().__init__(group)
         self.image = pygame.transform.scale(load_image('Player.png'), (width * 0.3, height * 0.3))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = round(0.35 * width), round(0.72 * height)
-        self.left = False
-        self.right = False
+        self.left = False  # плывем влево
+        self.right = False  # плывем вправо
 
-    def gun_shot(self):
+    def gun_shot(self):  # функция выстрела из пушки
         pass
 
-    def update(self, *args):
+    def update(self, *args):  # изменение местоположения
         if self.right:
             self.rect.x += 4
             if self.rect.x + self.rect.w > width:
@@ -57,10 +59,24 @@ class Player(Ship):
             if self.rect.x < 0:
                 self.rect.x = 0
 
+        if self.armor <= 0:
+            pass  # заканчиваем игру
 
-class Enemy(Ship):
-    def __init__(self, group, ship_type=None):
+
+class Enemy(Ship):  # класс врага
+    params = {'Канонерка': (), 'Крейсер': (),
+              'Линкор': (), 'Эсминец': ()}
+    # каждому виду корабля соответствуют свои характеристики: armor, speed, направление (вправо плывет или влево)
+
+    def __init__(self, group, ship_type):
         super().__init__(group)
+        self.info = self.params[ship_type]
+        # появляется за экраном
+        # выпускает торпеду с периодом ок. 3-6 секунд
+        # каждому типу соответствует своя картинка (см. Images)
+
+    def update(self):  # перемещение корабля
+        pass
 
 
 class Torpedo(pygame.sprite.Sprite):
@@ -88,10 +104,9 @@ class Torpedo(pygame.sprite.Sprite):
     def update(self, group):
         for sprite in group:
             if pygame.sprite.collide_mask(self, sprite):
-                print('no')
+                sprite.get_damage(80)
         else:
             if self.y < 0.25 * height or self.x < -0.05 * width or self.x > 1.05 * width or self.y > height:
-                print('ok')
                 self.kill()
             self.x += self.delta_x
             self.y += self.delta_y
@@ -99,14 +114,18 @@ class Torpedo(pygame.sprite.Sprite):
             self.rect.y = self.y
 
 
-class Battlefield:
+class Battlefield:  # игровое поле
     def __init__(self, difficulty=None, conditions=None):
-        self.bg = pygame.transform.scale(load_image('img.png'), screen.get_size())
+        # чем выше сложность, тем выше будет скорость всего происходящего
+        # погодные условия потом
+        self.bg = pygame.transform.scale(load_image('img.png'), screen.get_size())  # фоновое изображение
         self.ship_group = pygame.sprite.Group()
-        self.torpedo_group = pygame.sprite.Group()
+        self.torpedo_group = pygame.sprite.Group()  # группы спрайтов
         self.shoal_group = pygame.sprite.Group()
-        self.player = Player(self.ship_group)
+        self.player = Player(self.ship_group)  # игрок
         running = True
+        self.random_event = pygame.USEREVENT + 1  # событие генерации событий
+        pygame.time.set_timer(self.random_event, random.randint(1, 3) * 1000, 1)  # первое событие через 1-3 с
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -117,35 +136,61 @@ class Battlefield:
                     if event.key == pygame.K_RIGHT:
                         self.player.right = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT:  # для перемещения игрока по сторонам
                         self.player.left = False
                     if event.key == pygame.K_RIGHT:
                         self.player.right = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN:  # ПКМ - пуск торпеды
                     if event.button == 3:
-                        if event.pos[1] < height * 0.6:
+                        if event.pos[1] < height * 0.6:  # но вбок нельзя
                             self.player.torpedo_shot(event.pos, self.torpedo_group)
+                if event.type == self.random_event:  # генерация случайного события
+                    generated_event = random.choice(['мель', 'корабль'])  # сделать более вероятным то или иное событие
+                    if generated_event == 'мель':
+                        self.spawn_shoal()  # спавнит мель
+                    else:
+                        self.spawn_enemy()  # спавнит мель корабль врага
+                    pygame.time.set_timer(self.random_event, random.randint(7, 11) * 1000, 1)  # новое событие 7-11 с
             pygame.display.flip()
             screen.blit(self.bg, (0, 0))
             self.ship_group.update()
             self.ship_group.draw(screen)
-            self.torpedo_group.update(self.ship_group)
+            self.torpedo_group.update(self.ship_group)  # обновление групп спрайтов
             self.torpedo_group.draw(screen)
+            self.shoal_group.update(self.player)
+            self.shoal_group.draw(screen)
 
-    def spawn_shoal(self):
-        pass
+    def spawn_shoal(self):  # функция спавна мели
+        Shoal((random.random() * width, 0.3 * height),
+              (random.uniform(0.05, 0.3) * width, random.uniform(0.05, 0.2) * height), self.shoal_group)
+
+    def spawn_enemy(self):  # функция спавна корабля
+        Enemy(self.ship_group,
+              ship_type=random.choice(
+                  ['Канонерка', 'Эсминец', 'Линкор', 'Крейсер']))
 
 
-class Shoal(pygame.sprite.Sprite):
+class Shoal(pygame.sprite.Sprite):  # класс мели
     def __init__(self, coords: tuple, size: tuple, group):
         super().__init__(group)
-        self.size = self.width, self.height = size
-        self.coords = self.x, self.y = coords
+        self.image = pygame.transform.scale(load_image('shoal.png'), size)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = coords
+
+    def update(self, player):  # надо сделать так, чтобы из точки вырастал остров
+        if pygame.sprite.collide_mask(self, player):  # проверка удара
+            player.get_damage(40)
+            self.kill()
+            print('Получено 40 урона')
+        else:
+            self.rect.y += height * 0.001  # спуск вниз
+            if self.rect.y >= 0.96 * height:  # выход за границу
+                self.kill()
 
 
 if __name__ == '__main__':
     pygame.init()
-    FPS = 60
+    FPS = 100
     full_width, full_height = pygame.display.Info().current_w, pygame.display.Info().current_h
     size = width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
     screen = pygame.display.set_mode(size)
